bc4ffe0caa3f0353d5f937d7a21432d404b59c3c
willcray <will.c.cray@gmail.com>
2019-10-09 16:52:00 -0300

    train baseline accomplished. Adding data aug with update color jitter params

--- homework3/homework/utils.py
+++ homework3/homework/utils.py
@@ -70,7 +70,7 @@ class DenseSuperTuxDataset(Dataset):
 def load_data(dataset_path, num_workers=0, batch_size=128, **kwargs):
     transform = transforms.Compose([
         transforms.ToPILImage(),
-        transforms.ColorJitter(),
+        transforms.ColorJitter(brightness=1, contrast=1, saturation=1, hue=(-0.5,0.5)),
         transforms.RandomHorizontalFlip(),
         transforms.RandomRotation(20, resample=Image.BILINEAR),
         transforms.ToTensor()
