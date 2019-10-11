402dd35bc0be2b88674a9c102c93595439358764
willcray <will.c.cray@gmail.com>
2019-10-11 00:12:13 -0300

    fcn working but not accurate

--- homework3/homework/models.py
+++ homework3/homework/models.py
@@ -56,47 +56,87 @@ class CNNClassifier(torch.nn.Module):
         return self.classifier(z)
 
 class FCN(torch.nn.Module):
-    def __init__(self, n_input_channels=3):
+  class Block(torch.nn.Module):
+    def __init__(self, n_input, n_output, index, stride=1):
         super().__init__()
-        # block 1
-        num_classes = 5
-        kernel_size = 7
-        padding = kernel_size // 2
-        conv1 = torch.nn.Conv2d(n_input_channels, 16, kernel_size=kernel_size, padding=padding, stride=1, bias=False)
-        bn1 = torch.nn.BatchNorm2d(16)
-        relu1 = torch.nn.ReLU(inplace=True)
+        L = [
+            torch.nn.Conv2d(n_input, n_output, kernel_size=3,
+                            padding=1, stride=stride, bias=False),
+            torch.nn.BatchNorm2d(n_output),
+            torch.nn.ReLU(inplace=True),
+        ]
+        if index < 2:
+            L.append(torch.nn.MaxPool2d(kernel_size=3, stride=1, padding=1))
 
-
-        #  block 2
-        kernel_size = 3
-        padding = kernel_size // 2
-        conv2 = torch.nn.Conv2d(16, num_classes, kernel_size=kernel_size, padding=padding, stride=1, bias=False)
-        bn2 = torch.nn.BatchNorm2d(num_classes)
-        relu2 = torch.nn.ReLU(inplace=True)
-
-        L = [conv1, bn1, relu1, conv2, bn2, relu2]
         self.net = torch.nn.Sequential(*L)
         """
-        Your code here.
-        Hint: The FCN can be a bit smaller the the CNNClassifier since you need to run it at a higher resolution
-        Hint: Use up-convolutions
-        Hint: Use skip connections
-        Hint: Use residual connections
-        Hint: Always pad by kernel_size / 2, use an odd kernel_size
+        self.downsample = None
+        if stride != 1 or n_input != n_output:
+            self.downsample = torch.nn.Sequential(torch.nn.Conv2d(n_input, n_output, kernel_size=1, stride=stride, padding=1),
+                                                    torch.nn.BatchNorm2d(n_output))
         """
 
     def forward(self, x):
         """
-        Your code here
-        @x: torch.Tensor((B,3,H,W))
-        @return: torch.Tensor((B,6,H,W))
-        Hint: Apply input normalization inside the network, to make sure it is applied in the grader
-        Hint: Input and output resolutions need to match, use output_padding in up-convolutions, crop the output
+        identity = x
+        if self.downsample is not None:
+            identity = self.downsample(identity)
+        return self.net(x) + identity
+        """
+        return self.net(x)
+
+  def __init__(self, layers=[32, 94], n_input_channels=3):
+      super().__init__()
+      num_classes = 5
+      L = [torch.nn.Conv2d(n_input_channels, layers[0], kernel_size=7, padding=3, stride=2, bias=False),
+           torch.nn.BatchNorm2d(layers[0]),
+           torch.nn.ReLU(inplace=True),
+           torch.nn.MaxPool2d(kernel_size=3, stride=1, padding=1)
+           ]
+      c = layers[0]
+      for i, l in enumerate(layers):
+          L.append(self.Block(c, l, i, stride=2))
+          c = l
+
+      # output channels layer
+      L.append(torch.nn.Conv2d(layers[-1], num_classes, kernel_size=1))
+      L.append(torch.nn.BatchNorm2d(num_classes))
+
+      # final layer
+      L.append(torch.nn.ConvTranspose2d(
+          5, 5, kernel_size=4, stride=2, padding=1))
+      L.append(torch.nn.BatchNorm2d(num_classes))
+      L.append(torch.nn.ConvTranspose2d(
+          5, 5, kernel_size=4, stride=2, padding=1))
+      L.append(torch.nn.BatchNorm2d(num_classes))
+      L.append(torch.nn.ConvTranspose2d(
+          5, 5, kernel_size=4, stride=2, padding=1))
+      L.append(torch.nn.BatchNorm2d(num_classes))
+
+      self.net = torch.nn.Sequential(*L)
+      """
+      Your code here.
+      Hint: The FCN can be a bit smaller the the CNNClassifier since you need to run it at a higher resolution
+      Hint: Use up-convolutions
+      Hint: Use skip connections
+      Hint: Use residual connections
+      Hint: Always pad by kernel_size / 2, use an odd kernel_size
+      """
+
+  def forward(self, x):
+      """
+      Your code here
+      @x: torch.Tensor((B,3,H,W))
+      @return: torch.Tensor((B,6,H,W))
+      Hint: Apply input normalization inside the network, to make sure it is applied in the grader
+      Hint: Input and output resolutions need to match, use output_padding in up-convolutions, crop the output
             if required (use z = z[:, :, :H, :W], where H and W are the height and width of a corresponding strided
             convolution
-        """
-        z = self.net(x)
-        return z
+      """
+      z = self.net(x)
+      return z
+
+
 
 
 model_factory = {
